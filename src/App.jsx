37158c1c7594a5a6bbdf474801c9fdcff1242d26
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-03-15 10:04:48 +0600

    create react project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {Suspense} from 'react'
+import Countries from './components/countries/countries'
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
+.then(res => res.json())
+function App() {
+  
+  return (
+    <>
+      <Suspense fallback={<p>I am loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {use} from 'react'
+const Countries = ({countriesPromise}) => {
+  const countriesData = use(countriesPromise)
+  const countries = countriesData.countries;
+  return (
+    <div>
+      <h1>In the countries: {countries.length}</h1>
+    </div>
+    );
+};
+
+export default Countries;
