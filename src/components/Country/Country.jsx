740dc21572585e12638a80a36d1da4f09c222107
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-03-16 22:54:04 +0600

    added toggle button in card

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({ country, handleVisitedCountries }) => {
   const [visited, setVisited] = useState(false);
   
   const handleVisited = () => {
   //  setVisited(true);
   setVisited(!visited)
+  handleVisitedCountries(country);
   }
   return (
     <div className={`country ${visited && 'country-visited'}`}>
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,15 +1,26 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css';
 const Countries = ({countriesPromise}) => {
+  const [visitedCountries, setVisitedCountries] = useState([])
+  const handleVisitedCountries = (country) => {
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  }
+  
   const countriesData = use(countriesPromise)
   const countries = countriesData.countries;
   return (
     <div>
       <h1>In the countries: {countries.length}</h1>
+      <h3>Visited countries: {visitedCountries.length}</h3>
       <div className="countries">
        {
-        countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+        countries.map(country => <Country 
+        key={country.cca3} 
+        country={country} 
+        handleVisitedCountries={handleVisitedCountries}
+        ></Country>)
        }
       </div>
     </div>
